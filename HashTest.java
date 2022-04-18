60b95e6bc37f6886e3a0c126c4a64c124780c988
Vu Tan Le <imleeu@gmail.com>
2022-04-18 09:38:26 +0800

    Create HashTest.java

---
+++ HashTest.java
@@ -0,0 +1,24 @@
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+
+class Scratch {
+    public static void main(String[] args) {
+        char[] password = "testString123".toCharArray();
+        byte[] salt = new byte[16];
+        new SecureRandom().nextBytes(salt);
+        PBEKeySpec spec = new PBEKeySpec(password, salt, 10000, 256);
+        Arrays.fill(password, Character.MIN_VALUE);
+        try {
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            System.out.println(new String(skf.generateSecret(spec).getEncoded()));
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            throw new AssertionError("Error while hashing a password: " + e.getMessage(), e);
+        } finally {
+            spec.clearPassword();
+        }
+    }
+}
