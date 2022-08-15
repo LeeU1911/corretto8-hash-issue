4db7f7de3aeeebabbde6f591280a55176ae23e52
Vu Tan Le <imleeu@gmail.com>
2022-08-15 17:42:20 +0800

    Update HashTest.java

--- HashTest.java
+++ HashTest.java
@@ -1,15 +1,13 @@
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.util.Arrays;
 
 class HashTest {
     public static void main(String[] args) {
-        char[] password = "testString123".toCharArray();
-        byte[] salt = new byte[16];
-        new SecureRandom().nextBytes(salt);
+        char[] password = "abc123456".toCharArray();
+        byte[] salt = {74, 105, 85, -30, -110, -53, -23, -100, 34, 13, 39, 113, 0, 98, -24, 115};
         PBEKeySpec spec = new PBEKeySpec(password, salt, 10000, 256);
         Arrays.fill(password, Character.MIN_VALUE);
         try {
